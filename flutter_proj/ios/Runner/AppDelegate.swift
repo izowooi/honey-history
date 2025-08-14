7b391fe6af909f8aa48d07a4029cdb633048ebc6
jongwoo <izowooi@hotmail.com>
2025-08-14 22:26:42 +0900

    ios 빌드 정상화

--- flutter_proj/ios/Runner/AppDelegate.swift
+++ flutter_proj/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import Firebase
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,10 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Ensure Firebase is configured on iOS before Flutter-side initializeApp()
+    if FirebaseApp.app() == nil {
+      FirebaseApp.configure()
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
